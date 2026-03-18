c9d10dac2abe73fefeea06314a4a492184ad53cc
santi1475 <santiguz1475@gmail.com>
2026-03-18 18:10:36 -0500

    Fix: Correcciones de ENV

--- core/settings/development.py
+++ core/settings/development.py
@@ -3,14 +3,15 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
-# Base de datos local
+# Base de datos
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'postgres://santiago:1475@db:5432/saas_db'),
         conn_max_age=600,
-        ssl_require=False
+        ssl_require=False,
     )
 }
+
+DATABASES['default']['OPTIONS'] = {'options': '-c search_path=public'}
 
 cors_hosts = os.environ.get("CORS_ALLOWED_ORIGINS", "http://localhost:3000,http://127.0.0.1:3000")
 CORS_ALLOWED_ORIGINS = cors_hosts.split(",")
